1f2dbc95c4152cf0c65a7a4929aed44635013072
Rohitgaikwad2000 <153388725+Rohitgaikwad2000@users.noreply.github.com>
2024-01-28 20:37:20 +0530

    upload file

---
+++ Git/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+# Create your models here.
+
+class User(models.Model):
+    name = models.CharField(max_length = 50)
+    email = models.EmailField()
+    pasword = models.CharField(max_length = 50)
+
+    def __str__(self):
+        return f"{self.name}"
+    
+
+    class Meta:
+        db_table = "User"
+
+
+
+
